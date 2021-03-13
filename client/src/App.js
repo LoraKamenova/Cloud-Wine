bc1843b2da38fa3cb2d7779425036e6eec584531
Lora <cruzeiro@abv.bg>
2021-03-13 22:43:00 +0200

    Added header on home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import Home from "./components/Home/Home";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import './Home.css'
+import Header from "../Header/Header";
+
+const Home = () => {
+    return (
+        <main className="home-page">
+            <Header />
+        </main>
+    );
+};
+
+export default Home;
