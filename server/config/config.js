df6162f36ddd7f832396aedd8046eb8123d06816
Lora <cruzeiro@abv.bg>
2021-03-20 20:02:03 +0200

    Server initial commit

---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+    PORT: 5000,
+    DB_URI: `mongodb://localhost/wine-db`,
+    SALT_ROUNDS: 10,
+    SECRET: 'MNOGOQKASOL',
+    COOKIE_NAME: 'TOKEN',
+};
+
+
+module.exports = config;
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const routes = require('./config/routes');
+const { auth } = require('./middlewares/auth');
+const errorHandler = require('./middlewares/errorHandler');
+const { PORT } = require('./config/config')
+
+const app = express();
+
+require('./config/mongoose')
+
+app.use(cors());
+app.use(express.json()); // IMPORTANT FOR REST API!!!
+
+app.use(auth);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'It\'s working!'});
+});
+
+app.use('/api', routes);
+app.use(errorHandler);
+
+app.listen(PORT, console.log.bind(console, `Server is listening on port ${PORT}...`));
