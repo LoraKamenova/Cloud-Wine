62dda206a9c6f15ae5af226f650db38cdb3c7b7c
Lora <cruzeiro@abv.bg>
2021-03-20 02:30:27 +0200

    Change of destination detail card

--- client/src/components/AllDestinations/DestinationCard/DestinationCard.js
+++ client/src/components/AllDestinations/DestinationCard/DestinationCard.js
@@ -6,10 +6,24 @@ const DestinationCard = (props) => {
             <div className="destination-card-image-wrapper">
                 <img className="destination-card-image" src={props.url}/>
             </div>
+
             <div className="destination-card-info-wrapper">
-                <h3 className="destination-info-item destination-card-title"><i className="fab fa-houzz"></i>{props.title}</h3>
-                <h3 className="destination-info-item destination-card-address"><i className="fas fa-map-pin"></i>{props.address}</h3>
-                <h3 className="destination-info-item destination-card-services"><i className="fas fa-wine-glass-alt"></i>{props.services}</h3>
+
+                <div className="destination-card-line-wrapper">
+                    <img className="destination-card-pic" src="https://res.cloudinary.com/dyhmxus4n/image/upload/v1616189586/React%20Project/house_nwweqf.png" />
+                    <h3 className="destination-info-item destination-card-title">{props.title}</h3>
+                </div>
+
+                <div className="destination-card-line-wrapper">
+                    <img className="destination-card-pic" src="https://res.cloudinary.com/dyhmxus4n/image/upload/v1616189587/React%20Project/push-pin_hopc61.png" />
+                    <h3 className="destination-info-item destination-card-address">{props.address}</h3>
+                </div>
+
+                <div className="destination-card-line-wrapper">
+                    <img className="destination-card-pic" src="https://res.cloudinary.com/dyhmxus4n/image/upload/v1616199816/React%20Project/info_rg32io.png" />
+                    <h3 className="destination-info-item destination-card-services">{props.services}</h3>
+                </div>
+
             </div>
         </article>
     );
