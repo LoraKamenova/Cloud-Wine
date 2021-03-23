71551637daba05f017886745a0741230efc98623
Lora <cruzeiro@abv.bg>
2021-03-23 18:23:22 +0200

    Changed journal details card

--- client/src/components/JournalDetails/JournalDetailsCard/JournalDetailsCard.js
+++ client/src/components/JournalDetails/JournalDetailsCard/JournalDetailsCard.js
@@ -4,34 +4,36 @@ const JournalDetailsCard = (props) => {
     return (
         <div className="journal-details-card">
 
-            <header className="journal-details-card-header">
-                <h1 className="journal-details-card-heading">{props.title}</h1>
-            </header>
+            <section className="jdc-first-section">
+                    <h1 className="jdc-heading">{props.title}</h1>
 
-            <div className="journal-details-card-wrapper">
-                <h3 className="journal-details-card-subheading">Къде ходихме</h3>
-                <div className="journal-details-card-text">{props.place}</div>
-            </div>
-
-            <div className="journal-details-card-wrapper">
-                <h3 className="journal-details-card-subheading">Кога отидохме</h3>
-                <div className="journal-details-card-text">{props.time}</div>
-            </div>
-
-            <div className="journal-details-card-wrapper">
-                <h3 className="journal-details-card-subheading">Какво правихме</h3>
-                <div className="journal-details-card-text">{props.description}</div>
-            </div>
-
-            <div className="journal-details-card-image-wrapper">
-                <h3 className="journal-details-card-subheading description-heading">Какво снимахме</h3>
-                <div className="journal-details-card-image-container">
-                    <img className="journal-details-card-image" src={props.imageUrl1} alt="journal"/>
-                    <img className="journal-details-card-image" src={props.imageUrl2} alt="journal"/>
+                <div className="jdc-subsection">
+                    <h3 className="jdc-subheading">Къде ходихме</h3>
+                    <div className="jdc-text">{props.place}</div>
                 </div>
-            </div>
 
-            <div className="journal-details-card-button-wrapper">
+                <div className="jdc-subsection">
+                    <h3 className="jdc-subheading">Кога отидохме</h3>
+                    <div className="jdc-text">{props.time}</div>
+                </div>
+
+                <div className="jdc-subsection">
+                    <h3 className="jdc-subheading">Какво правихме</h3>
+                    <div className="jdc-text">{props.description}</div>
+                </div>
+            </section>
+
+            <section className="jdc-second-section">
+                <h3 className="jdc-subheading image-and-tape-heading">Какво снимахме</h3>
+                <div className="jdc-image-and-tape-wrapper">
+                    <div className="tape"></div>
+                    <img className="jdc-image" src={props.imageUrl1} alt="journal"/>
+                    <div className="tape"></div>
+                    <img className="jdc-image" src={props.imageUrl2} alt="journal"/>
+                </div>
+            </section>
+
+            <div className="jdc-button-wrapper">
                 <a className="button edit-button" href="#">Редактирай<i className="fas fa-pencil-alt"></i></a>
                 <a className="button delete-button" href="#">Изтрий<i className="fas fa-times"></i></a>
             </div>
