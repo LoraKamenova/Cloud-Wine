cbcbfec3c017c5112035950066a6064f34a2731d
Lora <cruzeiro@abv.bg>
2021-03-28 02:16:40 +0200

    added @media queries for responsive design

--- client/src/components/Shared/FormHeader/FormHeader.js
+++ client/src/components/Shared/FormHeader/FormHeader.js
@@ -6,10 +6,13 @@ const FormHeader = (props) => {
             <div className="form-header-heading-container">
                 <h1 className="form-header-heading">{props.title}</h1>
             </div>
+
             <div className="form-header-image-container">
                 <img className="form-header-image"
-                     src="https://res.cloudinary.com/dyhmxus4n/image/upload/v1615712293/React%20Project/grape_border_ogd8gq.png" alt="vine"/>
+                     src="https://res.cloudinary.com/dyhmxus4n/image/upload/v1615712293/React%20Project/grape_border_ogd8gq.png"
+                     alt="vine"/>
             </div>
+            <h1 className="form-header-heading-hidden">{props.title}</h1>
         </div>
     );
 };
